52b42566fb484c24a9e456553d3fdc4d6d080c60
designermonkey <john.porter@designermonkey.co.uk>
2011-04-03 14:43:38 +0100

    Updated to use the new key-less API

--- extension.driver.php
+++ extension.driver.php
@@ -1,7 +1,7 @@
 <?php
 
 	Class extension_maplocationfield extends Extension{
-	
+
 		public function about(){
 			return array('name' => 'Field: Map Location',
 						 'version' => '3.1',
@@ -11,44 +11,9 @@
 										   'email' => 'team@symphony21.com')
 				 		);
 		}
-		
+
 		public function uninstall(){
 			Symphony::Database()->query("DROP TABLE `tbl_fields_maplocation`");
-			Symphony::Configuration()->remove('google-api-key', 'map-location-field');			
-			Administration::instance()->saveConfig();
-		}
-		
-		public function getSubscribedDelegates(){
-			return array(
-						array(
-							'page' => '/system/preferences/',
-							'delegate' => 'AddCustomPreferenceFieldsets',
-							'callback' => 'appendPreferences'
-						),
-						array(
-							'page'		=> '/frontend/',
-							'delegate'	=> 'FrontendParamsResolve',
-							'callback'	=> 'frontendParamsResolve'
-						)
-					);
-		}		
-
-		public function appendPreferences($context) {
-			$group = new XMLElement('fieldset');
-			$group->setAttribute('class', 'settings');
-			$group->appendChild(new XMLElement('legend', 'Map Location Field'));
-
-			$label = Widget::Label('Google Maps API Key');
-			$label->appendChild(Widget::Input('settings[map-location-field][google-api-key]', General::Sanitize(Symphony::Configuration()->get('google-api-key', 'map-location-field'))));		
-			$group->appendChild($label);
-			
-			$group->appendChild(new XMLElement('p', 'Get a Google Maps API key from the <a href="http://code.google.com/apis/maps/index.html">Google Maps site</a>.', array('class' => 'help')));
-			
-			$context['wrapper']->appendChild($group);						
-		}
-		
-		public function frontendParamsResolve($context) {
-			$context['params']['google-api-key'] = Symphony::Configuration()->get('google-api-key', 'map-location-field');
 		}
 
 		public function install() {
@@ -62,15 +27,15 @@
 			  UNIQUE KEY `field_id` (`field_id`)
 			) TYPE=MyISAM");
 		}
-		
+
 		/*
 			Modified from:
 			http://www.kevinbradwick.co.uk/developer/php/free-to-script-to-calculate-the-radius-of-a-coordinate-using-latitude-and-longitude
 		*/
 		public function geoRadius($lat, $lng, $rad, $kilometers=false) {
 			$radius = ($kilometers) ? ($rad * 0.621371192) : $rad;
-			
-			(float)$dpmLAT = 1 / 69.1703234283616; 
+
+			(float)$dpmLAT = 1 / 69.1703234283616;
 
 			// Latitude calculation
 			(float)$usrRLAT = $dpmLAT * $radius;
@@ -81,40 +46,40 @@
 			(float)$mpdLON = 69.1703234283616 * cos($lat * (pi/180));
 			(float)$dpmLON = 1 / $mpdLON; // degrees per mile longintude
 			$usrRLON = $dpmLON * $radius;
-			
+
 			$lonMIN = $lng - $usrRLON;
 			$lonMAX = $lng + $usrRLON;
-			
+
 			return array("lonMIN" => $lonMIN, "lonMAX" => $lonMAX, "latMIN" => $latMIN, "latMAX" => $latMAX);
 		}
-		
+
 		/*
 		Calculate distance between two lat/long pairs
 		*/
-		public function geoDistance($lat1, $lon1, $lat2, $lon2, $unit) { 
+		public function geoDistance($lat1, $lon1, $lat2, $lon2, $unit) {
 
-			$theta = $lon1 - $lon2; 
-			$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)); 
-			$dist = acos($dist); 
-			$dist = rad2deg($dist); 
+			$theta = $lon1 - $lon2;
+			$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+			$dist = acos($dist);
+			$dist = rad2deg($dist);
 			$miles = $dist * 60 * 1.1515;
 
 			$unit = strtolower($unit);
-			
+
 			$distance = 0;
-			
+
 			if ($unit == "k") {
-				$distance = ($miles * 1.609344); 
+				$distance = ($miles * 1.609344);
 			} else if ($unit == "n") {
 				$distance = ($miles * 0.8684);
 			} else {
 				$distance = $miles;
 			}
-			
+
 			return round($distance, 1);
-			
+
 		}
-			
+
 	}
 
 ?>
